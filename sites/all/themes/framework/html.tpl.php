234b359697ee45f9ed05cae89a1a9e4406d4a232
Nando Alvarado <nandoalvarado022@gmail.com>
2018-02-10 17:55:24 -0500

    ajuste multimedia y logo

--- sites/all/themes/framework/html.tpl.php
+++ sites/all/themes/framework/html.tpl.php
@@ -30,7 +30,7 @@ $base_theme=path_to_theme();
 	<script src="<?php echo $base_url?>/scripts/mi.js"></script>
 
 	<!-- livereload -->
-		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+		<!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
 	<!-- livereload -->
 
 	<!-- Adsense -->
