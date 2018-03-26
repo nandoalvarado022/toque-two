06648a6ccd7aea91cf46ebf39436a663e273410b
Nando Alvarado <nandoalvarado022@gmail.com>
2018-03-25 20:44:13 -0500

    first

--- sites/all/themes/framework/user-profile.tpl.php
+++ sites/all/themes/framework/user-profile.tpl.php
@@ -16,7 +16,7 @@
 <!-- view -->
 <h1><?php print $user_data->name?></h1>
 
-<div class="top">
+<div class="top oh">
     <img id="img_profile" src="<?php echo image_style_url('350x550', $picture_uri)?>">
     <div class="description">
         <?php echo $descripcion?>
@@ -30,15 +30,17 @@
 <div class="bottom">
     <?php print views_get_view('usuarios')->preview('block_2');?>
 
-    <!-- 300x600_1 -->
-    <div class="pauta">
-        <ins class="adsbygoogle"
-                style="display:inline-block;width:300px;height:600px"
-                data-ad-client="ca-pub-4733150240823723"
-                data-ad-slot="5401552695"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+    <div class="sidebar_der desktop">
+        <!-- 300x600_1 -->
+        <div class="pauta">
+            <ins class="adsbygoogle"
+                    style="display:inline-block;width:300px;height:600px"
+                    data-ad-client="ca-pub-4733150240823723"
+                    data-ad-slot="5401552695"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     </div>
 
 </div>
